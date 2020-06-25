305e4cb61c46fd1e2ec8680a6a5971903951c018
Suthep Chanchuphol <suthep.chanchuphol@outlook.com>
2020-06-25 22:30:22 +0700

    Add popup message when QR is not correct

--- src/views/qrcodeReader.vue
+++ src/views/qrcodeReader.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="qr">
-    <div class="reader" style="width:100%;height:calc(100vh - 52px)">
+    <div class="reader" style="height:calc(100vh - 52px)">
       <qrcode-stream @decode="onDecode"></qrcode-stream>
     </div>
-    <h2 class="text-center">{{ msg }}</h2>
+    <div v-if="was_incorrect" class="toast-overlay">
+      <i class="fas fa-exclamation-triangle"></i> QR Code ไม่ถูกต้อง
+      กรุณาลองใหม่
+    </div>
   </div>
 </template>
 
@@ -17,15 +20,13 @@ export default {
   },
   data() {
     return {
-      msg: ""
+      was_incorrect: false
     };
   },
   methods: {
     onDecode(result) {
       console.log(result);
       if (result.indexOf("qr.thaichana.com") !== -1) {
-        this.msg = "QRcode ถูกต้อง";
-
         const url = new URL(result);
         const params = new URLSearchParams(url.search);
         let appId = params.get("appId");
@@ -38,22 +39,39 @@ export default {
 
         this.$router.push("/shop_detail");
       } else {
-        this.msg = "QRcode ไม่ถูกต้อง";
+        this.was_incorrect = true;
       }
+    }
+  },
+  watch: {
+    was_incorrect() {
+      setTimeout(() => (this.was_incorrect = false), 5000);
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.text-center {
-  text-align: center;
+@keyframes slide_in {
+  0% {
+    bottom: -64px;
+  }
+  to {
+    bottom: 4px;
+  }
 }
 
-.reader {
-  width: 300px;
-  height: 300px;
-  margin-left: auto;
-  margin-right: auto;
+.toast-overlay {
+  position: fixed;
+  bottom: 4px;
+  left: 4px;
+  right: 4px;
+  color: #856404;
+  background-color: #fff3cd;
+  border-color: #ffeeba;
+  padding: 0.75rem 1.25rem;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  animation: slide_in 0.5s forwards;
 }
 </style>
