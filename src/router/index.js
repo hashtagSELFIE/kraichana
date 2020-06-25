fce90234704ab06facf4d5ff4d50421a1dbe2ebb
Suthep Chanchuphol <suthep.chanchuphol@outlook.com>
2020-06-25 17:24:29 +0700

    Make route view lazy

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,48 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
+
+function loadView(view) {
+  return () => import(`../views/${view}.vue`);
+}
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: loadView("Home"),
+    meta: {
+      title: "Home",
+    },
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    component: loadView("About"),
+    meta: {
+      title: "About",
+    },
+  },
+  {
+    path: "*",
+    redirect: "/",
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const customTitle = to.matched
+    .slice()
+    .reverse()
+    .find((record) => record.meta && record.meta.title);
+  document.title = `E-choice - ${customTitle ? customTitle.meta.title : ""}`;
+  next();
 });
 
 export default router;
