b0e1bd94adf8d6c31a18fc39814232dd5f7bb4ec
Suthep Chanchuphol <suthep.chanchuphol@outlook.com>
2020-06-25 18:12:46 +0700

    Add app-navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <app-navbar />
     <router-view />
   </div>
 </template>
 
 <script>
+import AppNavbar from "@/components/AppNavbar.vue";
+
 export default {
   name: "App",
+  components: {
+    AppNavbar
+  }
 };
 </script>
 
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="main-navbar" role="navigation" aria-label="main navigation">
+    <section class="home">
+      <router-link to="/"><i class="fas fa-home fa-2x"></i></router-link>
+    </section>
+    <section class="title">
+      ใครชนะ
+    </section>
+    <section class="qr">
+      <router-link to="/about"><i class="fas fa-qrcode fa-2x"></i></router-link>
+    </section>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppNavbar"
+};
+</script>
+
+<style lang="scss" scoped>
+.main-navbar {
+  display: flex;
+  align-items: center;
+  height: 52px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background: #fff;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.15);
+  padding: 0 1rem;
+
+  > .title {
+    text-align: center;
+    flex: 1 1 0;
+    font-size: 1.5rem;
+    font-weight: 700;
+  }
+
+  > .home,
+  > .qr {
+    > a {
+      width: 36px;
+      height: 36px;
+      color: #444;
+      text-decoration: none;
+      position: relative;
+      bottom: -3px;
+    }
+  }
+}
+</style>
