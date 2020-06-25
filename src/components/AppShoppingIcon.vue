157528ba87eab6ba283ed3525ea479f0a4d42832
Suthep Chanchuphol <suthep.chanchuphol@outlook.com>
2020-06-25 21:49:02 +0700

    Add shopping icon

---
+++ src/components/AppShoppingIcon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div color="#9BD126" size="57" class="css-bcjcx5 e1ypfm4x0">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path
+        d="M19 6h-2c0-2.76-2.24-5-5-5S7 3.24 7 6H5c-1.1 0-1.99.9-1.99 2L3 20c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-7-3c1.66 0 3 1.34 3 3H9c0-1.66 1.34-3 3-3zm0 10c-2.76 0-5-2.24-5-5h2c0 1.66 1.34 3 3 3s3-1.34 3-3h2c0 2.76-2.24 5-5 5z"
+        fill="#fff"
+      ></path>
+    </svg>
+  </div>
+</template>
+
+<style scoped>
+.css-bcjcx5 {
+  width: 57px;
+  height: 57px;
+  border-radius: 57px;
+  background-color: #9bd126;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+}
+</style>
