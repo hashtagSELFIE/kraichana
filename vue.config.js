e509eda6f32fea66132917a1112ce5392263050c
C.Traivinidsreesuk <james333james.ct@gmail.com>
2020-06-26 22:43:38 +0700

    Build for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // publicPath: process.env.NODE_ENV === "production" ? "/kraichana" : "/",
+  publicPath: "/kraichana",
+  outputDir: "docs",
   configureWebpack: {
     optimization: {
       splitChunks: {
