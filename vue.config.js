19798ebcceff45a609bc96dad8b84aa5eebae7d3
Suthep Chanchuphol <suthep.chanchuphol@outlook.com>
2020-06-26 23:32:29 +0700

    Fix image path, webpack manifest

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,20 @@ module.exports = {
         chunks: "all"
       }
     }
+  },
+  pwa: {
+    name: "ใครชนะ?",
+    themeColor: "#ffffff",
+    msTileColor: "#2d89ef",
+    iconPaths: {
+      favicon32: "favicon-32x32.png",
+      favicon16: "favicon-16x16.png",
+      appleTouchIcon: "apple-touch-icon.png",
+      maskIcon: "safari-pinned-tab.svg",
+      msTileImage: "mstile-150x150.png"
+    },
+    manifestOptions: {
+      background_color: "#ffffff"
+    }
   }
 };
